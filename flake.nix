967849cbc262afeb378af7eead093d6d8c94dd9e
Alexey Orlenko <alex@aqrln.net>
2023-10-12 15:40:30 +0200

    Add nix flake (#1)

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "PlanetScale proxy";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            go
+            gopls
+          ];
+        };
+      });
+}
